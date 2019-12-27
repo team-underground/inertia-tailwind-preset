adf79e2b8400c7ec4aae1b9950210b1f4209ce44
abhishek sarmah <abhisheksarmah660@gmail.com>
2019-12-27 12:13:45 +0530

    styles updated for usage of septemberui

--- src/Preset.php
+++ src/Preset.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Console\Presets\Preset as LaravelPreset;
 class Preset extends LaravelPreset
 {
     public static function install()
-    {   
+    {
         static::cleanSassDirectory();
         static::updatePackages();
         static::updateBootstrapping();
@@ -40,15 +40,28 @@ class Preset extends LaravelPreset
             'popper.js',
             'lodash',
             'jquery'
-        ])); 
+        ]));
     }
 
     public static function updateBootstrapping()
-    { 
+    {
         copy(__DIR__ . '/stubs/webpack.mix.js', base_path('webpack.mix.js'));
         copy(__DIR__ . '/stubs/resources/js/app.js', resource_path('js/app.js'));
         copy(__DIR__ . '/stubs/resources/sass/app.scss', resource_path('sass/app.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_button.scss', resource_path('sass/_button.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_datepicker.scss', resource_path('sass/_datepicker.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_file.scss', resource_path('sass/_file.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_fonts.scss', resource_path('sass/_fonts.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_form.scss', resource_path('sass/_form.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_listgroup.scss', resource_path('sass/_listgroup.scss'));
         copy(__DIR__ . '/stubs/resources/sass/_nprogress.scss', resource_path('sass/_nprogress.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_quillsnow.scss', resource_path('sass/_quillsnow.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_switch.scss', resource_path('sass/_switch.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_table.scss', resource_path('sass/_table.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_tags.scss', resource_path('sass/_tags.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_timepicker.scss', resource_path('sass/_timepicker.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_trendchart.scss', resource_path('sass/_trendchart.scss'));
+        copy(__DIR__ . '/stubs/resources/sass/_vuesnackbar.scss', resource_path('sass/_vuesnackbar.scss'));
     }
 
     protected static function updateWelcomePage()
